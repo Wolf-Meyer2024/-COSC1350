ad513aec79020c5e8b945b9afd1389dbaf5c8142
Wolf-Meyer2024 <wolfmeyer@westernwyoming.edu>
2024-09-24 15:03:40 -0600

    Add files via upload

---
+++ m05/practiceVariables.js
@@ -0,0 +1,28 @@
+let hi = "Hi Dr. T!"
+
+console.log(hi)
+
+let float = 4.5
+
+console.log(float)
+
+let int = 0
+
+console.log(int)
+
+let neg_Int = -10
+
+console.log(neg_Int)
+
+let boolean = true
+
+console.log(boolean)
+
+console.log("Hi Dr. T!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
